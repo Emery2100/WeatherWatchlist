f4a9c8586bb13de2759f88848371ba6a40fda6ff
David Emery <Emery2100@yahoo.com>
2026-04-20 20:12:21 -0700

    Initial Commit

---
+++ WeatherWatchlist/WeatherWatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWatchlistApp.swift
+//  WeatherWatchlist
+//
+//  Created by David Emery on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
